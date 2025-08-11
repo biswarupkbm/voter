590f973c6d1877e897f7ecf45c9f48583ea8dcde
biswarupkbm <biswarupkbm@gmail.com>
2025-08-11 16:38:28 +0530

    updated files

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index3');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/login', function () {
+    return view('contact');
 });
